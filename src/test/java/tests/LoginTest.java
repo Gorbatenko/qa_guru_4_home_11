0575aacdd81527b8daa238971cf566694bea0b84
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-03-30 01:44:49 +0300

    added allure annotations

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,10 +1,16 @@
 package tests;
 
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static io.qameta.allure.SeverityLevel.MINOR;
 
 public class LoginTest extends BaseTest{
 
@@ -15,31 +21,53 @@ public class LoginTest extends BaseTest{
         open(BASE_PATH);
     }
 
+    @AfterEach
+    void afterEach() {
+//        https://qa.guru/user/my/logout
+    }
+
+
+
     @Test
+    @Owner("GorbatenkoVA")
+    @Severity(CRITICAL)
+    @Story("Проверка авторизационной формы")
     @DisplayName("Логин с неправильной почтой не пропустит в личный кабинет.")
     void test6() {
 
     }
 
     @Test
+    @Owner("GorbatenkoVA")
+    @Severity(CRITICAL)
+    @Story("Проверка авторизационной формы")
     @DisplayName("Логин с неправильным паролем не пропустит в личный кабинет.")
     void test7() {
 
     }
 
     @Test
+    @Owner("GorbatenkoVA")
+    @Severity(CRITICAL)
+    @Story("Проверка авторизационной формы")
     @DisplayName("Логин с валидными данными ведёт в личный кабинет.")
     void test8() {
 
     }
 
     @Test
+    @Owner("GorbatenkoVA")
+    @Severity(MINOR)
+    @Story("Проверка редиректа главной страницы")
     @DisplayName("Авторизованного студента переводит в личный кабинет с главной страницы.")
     void test9() {
 
     }
 
     @Test
+    @Owner("GorbatenkoVA")
+    @Severity(MINOR)
+    @Story("Проверка редиректа главной страницы")
     @DisplayName("После выхода из личного кабинета главная страница доступна.")
     void test10() {
 
--- src/test/java/tests/QaGuruTest.java
+++ src/test/java/tests/QaGuruTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static io.qameta.allure.SeverityLevel.NORMAL;
 
 public class QaGuruTest extends BaseTest{
 
@@ -16,31 +17,45 @@ public class QaGuruTest extends BaseTest{
     }
 
     @Test
+    @Severity(NORMAL)
     @Owner("GorbatenkoVA")
+    @Story("Проверка общей информации")
     @DisplayName("Наличие всех заявленных инструментов в программе обучения")
     void test1() {
 
     }
 
     @Test
+    @Severity(NORMAL)
+    @Owner("GorbatenkoVA")
+    @Story("Проверка общей информации")
     @DisplayName("Наличие всех заявленных преподавателей в программе обучения")
     void test2() {
 
     }
 
     @Test
+    @Severity(CRITICAL)
+    @Owner("GorbatenkoVA")
+    @Story("Проверка контактов")
     @DisplayName("Кнопка 'Начать обучение' ведёт к записи на курс.")
     void test3() {
 
     }
 
     @Test
+    @Severity(NORMAL)
+    @Owner("GorbatenkoVA")
+    @Story("Проверка контактов")
     @DisplayName("Кнопка 'Как оплатить' ведёт в телеграмм.")
     void test4() {
 
     }
 
     @Test
+    @Severity(CRITICAL)
+    @Owner("GorbatenkoVA")
+    @Story("Проверка общей информации")
     @DisplayName("Цена обучения для разных уровней актуальна.")
     void test5() {
 
