d3db33c572a09b3576cb71352d05c64469fa8aae
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-04-07 00:36:48 +0300

    change addAttachments

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.EnvironmentConfig;
+import helpers.AttachmentsHelper;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
