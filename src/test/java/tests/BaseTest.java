4b0146ea1873b19fed4102d249595b1e5aba717d
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-04-09 22:25:12 +0300

    change addAttachments

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -39,9 +39,9 @@ public class BaseTest {
                     envConfig.getSelenoidPassword());
         }
 
-        setEnvironmentAllure("task", System.getProperty("TASK", "test"));
-        setEnvironmentAllure("browser", envConfig.getBrowser());
-        setEnvironmentAllure("platform", envConfig.getPlatform());
+//        setEnvironmentAllure("task", System.getProperty("TASK", "test"));
+//        setEnvironmentAllure("browser", envConfig.getBrowser());
+//        setEnvironmentAllure("platform", envConfig.getPlatform());
     }
 
     @AfterEach
