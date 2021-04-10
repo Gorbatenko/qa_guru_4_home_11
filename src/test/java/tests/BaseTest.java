a3073d8fd93cffefe97d307f3d9215647d8fe7ea
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-04-10 17:30:23 +0300

    add aspectjweaver

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,13 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.AssertionMode.SOFT;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 import static helpers.AttachmentsHelper.*;
 import static helpers.ConfigHelper.*;
 import static io.qameta.allure.Allure.step;
 
 public class BaseTest {
-
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
@@ -56,6 +54,4 @@ public class BaseTest {
             attachVideo(sessionId);
         }
     }
-
-
 }
